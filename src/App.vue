7a07fc984451e4c0954c8e8a630e3ab8c951ec1c
Saharien <mika@mkarl.de>
2021-05-02 00:20:24 +0200

    Router und Auth0 installiert

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <template>
-  <RankList/>
+  <!--
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  -->
+  <router-view/>
 </template>
-
-<script>
-import RankList from '@/components/RankList'
-
-export default {
-  name: 'App',
-  components: {
-    RankList
-  }
-}
-</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Nunito Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <RankList/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import RankList from '@/components/RankList.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    RankList
+  }
+}
+</script>
